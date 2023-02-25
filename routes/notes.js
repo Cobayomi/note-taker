bcb500544ff951dab6f2499b5762553619acb267
capricia <capriciaw16@gmail.com>
2023-02-25 14:41:37 -0600

    moved folder

--- routes/notes.js
+++ routes/notes.js
@@ -1,4 +1,10 @@
 const router = require('express').Router()
+const path = require("path");
+const fs = require("fs");
+
+const uniqid = require("uniqid");
+
+const dbPath = path.join(__dirname, "..", "db", "db.json");
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "..", "public", "index.html"))
@@ -6,6 +12,23 @@ router.get('/', (req, res) => {
 
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "..", "public", "notes.html"))
-})
+    fs.readFile(dbPath, "utf-8", function (err, data) {
+        if (err) {
+            res.status(500).json(err);
+            return;
+        }
+        const json = JSON.parse(data);
+        res.json(json);
+    });
+});
 
-module.exports = router
+router.post('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "notes.html"))
+    fs.readFile(dbPath, "utf-8", function (err, data) {
+        if (err) {
+            res.status(500).json(err);
+            return;
+        
+        const notes = JSON.parse(data);
+
+        module.exports = router;
