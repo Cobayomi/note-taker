99648d84686c108d43cac0406e076cb06d9fac82
capricia <capriciaw16@gmail.com>
2023-03-03 00:47:48 -0600

    changed routes due to errors

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const htmlRoutes = require("./html.js");
-const notesRoutes = require("./notes.js");
+const notesRoutes = require("./api.js");
 
 router.use("/api", notesRoutes);
 router.use(htmlRoutes);
--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-const express = require ('express')
-const app = express()
-const PORT = process.env.PORT || 3000;
+const express = require ('express');
+const fs = require('fs');
+const notes = require('./db/db.json');
+const public = require("./public")
+const path = require("path");
+const uuid = require("uuid");
 const routes = require("./routes")
-const path = require("path")
 
-console.log(__dirname)
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+
+//middleware//
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static("public"));
+app.use(express.json(public));
+
 
 app.get('/', (req, res) => {
   res.sendFile( path.join(_dirname, "public", "index.html"))
@@ -19,9 +31,6 @@ app.post('/', (req, res) => {
   res.sendFile( path.join(_dirname, "public", "notes.html"))
 })
 
-
-app.use(express.static("public"));
-app.use(express.json());
 
 app.use(routes);
 
